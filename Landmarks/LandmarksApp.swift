604a92f867111b2126c85f946f6b80b481160fea
pistolcaffe <pistolcaffe@naver.com>
2023-02-08 14:09:22 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by suminjin on 2023/02/08.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

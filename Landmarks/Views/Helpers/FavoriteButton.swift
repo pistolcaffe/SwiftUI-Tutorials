5740dde436ac1362517a8226c5d388bce7a89ee7
pistolcaffe <pistolcaffe@naver.com>
2023-02-10 17:57:50 +0900

    commit all contents

---
+++ Landmarks/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,28 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by suminjin on 2023/02/09.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
---
+++ Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -0,0 +1,70 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by suminjin on 2023/02/08.
+//
+
+import SwiftUI
+import MapKit
+
+struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
+    var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: {
+            $0.id == landmark.id
+        })!
+    }
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                MapView(coordinate: landmark.locationCoordinate)
+                    .ignoresSafeArea(edges: .top)
+                    .frame(height: 300)
+                
+                CircleImage(image: landmark.image)
+                    .offset(y: -130)
+                    .padding(.bottom, -130)
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text(landmark.name)
+                            .font(.title)
+                        FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                    }
+                    
+                    HStack {
+                        Text(landmark.park)
+                            .font(.subheadline)
+                        Spacer()
+                        Text(landmark.state)
+                            .font(.subheadline)
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    
+                    Divider()
+                    
+                    Text("About \(landmark.name)")
+                        .font(.title2)
+                    Text(landmark.description)
+                }
+                .padding()
+            }
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static let modelData = ModelData()
+    
+    static var previews: some View {
+        LandmarkDetail(landmark: modelData.landmarks[0])
+            .environmentObject(modelData)
+    }
+}
